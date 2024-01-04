fa439fe4ab37c554618a79750c8c0f1d20d44e04
hafsachenni <hafsachenni77@gmail.com>
2024-01-04 09:08:18 -0800

    height

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,41 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: tree to be measured
+ * Return: height of tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t depth_left, depth_right;
+
+	if (tree == NULL || (!tree->left && !tree->right))
+	{
+		return (0);
+	}
+
+	depth_left = binary_tree_height(tree->left);
+	depth_right = binary_tree_height(tree->right);
+
+	if (depth_left > depth_right)
+		return (depth_left + 1);
+	else
+		return (depth_right + 1);
+}
+/**
+ * binary_tree_is_perfect - unction that checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree
+ * Return: 0 if tree is null
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+		return (1);
+	return (0);
+}
